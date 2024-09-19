13014a28dbdb07af659fcecedd1357fc031157df
jayadusiya29 <jaya.dusiya.al23@ggits.net>
2024-09-19 21:49:44 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,16 +1,51 @@
-const express = require('express');
-const path = require('path');
+const http = require('http');
+const fs = require('fs');
 const minimist = require('minimist');
-const app = express();
-
-// Use minimist to get the port from command line
 const args = minimist(process.argv.slice(2));
-const port = args.port || 3000; // Default to port 3000 if not provided
 
-app.get('/registration', (req, res) => {
-    res.sendFile(path.join(__dirname, 'http-server', 'registration.html'));
+const port = parseInt(args.port);
+
+let homeContent = '';
+let projectContent = '';
+
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    console.log(err);
+  }
+  homeContent = home;
 });
 
-app.listen(port, () => {
-    console.log(Server running on port ${port});
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    console.log(err);
+  }
+  projectContent = project;
 });
+
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    console.log(err);
+  }
+  registrationContent = registration;
+});
+
+http
+  .createServer((req, res) => {
+    let url = req.url;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    switch (url) {
+      case '/project':
+        res.write(projectContent);
+        res.end();
+        break;
+      case '/registration':
+        res.write(registrationContent);
+        res.end();
+        break;
+      default:
+        res.write(homeContent);
+        res.end();
+        break;
+    }
+  })
+  .listen(port);
